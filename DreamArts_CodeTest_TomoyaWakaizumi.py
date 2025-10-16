76ed2a0d73bc96bf712be32020e55cc124fcf834
Tomoya <tomoyaw0319@gmail.com>
2025-10-16 17:40:07 +0900

    探索機能の追加

--- DreamArts_CodeTest_TomoyaWakaizumi.py
+++ DreamArts_CodeTest_TomoyaWakaizumi.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+import sys
 
 Q = []
 
@@ -17,7 +18,49 @@ for a, b, d in Q:
     nodes.add(a)
     nodes.add(b)
 
-print("ノード一覧:", nodes)
-print("隣接リスト:")
-for node, edges in adj.items():
-    print(f"{node} , {edges}")
+if not nodes:
+    sys.exit(0)
+
+best_len = -1.0
+best_path = []
+
+#最大値摘出
+max_out = {u: (max((w for _, w in adj[u]), default=0.0)) for u in nodes}
+
+for u in adj:
+    adj[u].sort(key=lambda x: x[1], reverse=True)
+
+sys.setrecursionlimit(1 << 25)
+
+def dfs(start, u, visited, path, cur_len, to_start=True):
+
+    global best_len, best_path
+
+    if cur_len > best_len:
+        best_len = cur_len
+        best_path = path[:]
+
+    if adj[u]:
+        optimistic = cur_len + adj[u][0][1]
+        if optimistic <= best_len:
+            return
+        
+    for v, w in adj[u]:
+        if v not in visited:
+            visited.add(v)
+            path.append(v)
+            dfs(start, v, visited, path, cur_len + w, to_start=to_start)
+            path.pop()
+            visited.remove(v)
+        elif to_start and v == start and len(path) > 1:
+            total = cur_len + w
+            if total > best_len:
+                best_len = total
+                best_path = path[:] + [start]
+
+#test
+for s in nodes:
+    dfs(s, s, {s}, [s], 0.0, to_start=True)
+
+print("best_len:", best_len)
+print("path:", " -> ".join(map(str, best_path)))

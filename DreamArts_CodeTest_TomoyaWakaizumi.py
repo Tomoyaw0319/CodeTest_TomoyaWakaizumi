66be776c0cb065853e86da359fd9596a758f63c4
Tomoya <tomoyaw0319@gmail.com>
2025-10-16 16:43:11 +0900

    有向グラフ化

--- DreamArts_CodeTest_TomoyaWakaizumi.py
+++ DreamArts_CodeTest_TomoyaWakaizumi.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 Q = []
 
 while True:
@@ -7,5 +9,15 @@ while True:
     a, b, d = map(str.strip, line.split(','))
     Q.append((int(a), int(b), float(d)))
 
-for f in Q:
-    print(f)
+
+adj = defaultdict(list)
+nodes = set()
+for a, b, d in Q:
+    adj[a].append((b, d))
+    nodes.add(a)
+    nodes.add(b)
+
+print("ノード一覧:", nodes)
+print("隣接リスト:")
+for node, edges in adj.items():
+    print(f"{node} , {edges}")
